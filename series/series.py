5d8bb50f5df6842dfb1af2e13a4515e23f61cec3
RHETT CHASE <rhettjchase@gmail.com>
2024-01-09 15:53:20 -0800

    adds lucas function and tests with all passing

--- series/series.py
+++ series/series.py
@@ -1,5 +1,39 @@
 def fibonacci(n):
+     """
+     Return the nth value of the Fibonacci series
+     
+     Parameters:
+     n (int): 1 integer
+     
+     Returns:
+     1 integer
+     int: For n = 0, return 0
+     int: For n = 1, return 1
+     int: For n > 1, call F(n-1) and F(n-2) and add them together
+     """
      # base case
      if n in {0, 1}:
           return n
      return fibonacci(n - 1) + fibonacci(n - 2) # Recursive case
+
+def lucas(n):
+     """
+     Return the nth value of the Lucas series
+     
+     Parameters:
+     n(int): 1 integer
+     
+     Returns:
+     1 integer
+     int: For n = 0, return 2
+     int: For n = 1, return 1
+     int: For n > 1, call L(n-1) and L(n-2) and add them together
+     """
+     # base case
+     if n == 0:
+          return 2
+     if n == 1:
+          return 1
+     return lucas(n - 1) + lucas(n - 2)
+     
+     
--- tests/test_series.py
+++ tests/test_series.py
@@ -1,5 +1,5 @@
 import pytest
-from series.series import fibonacci
+from series.series import fibonacci, lucas
 
 def test_series_zero():
     actual = fibonacci(0)
@@ -14,3 +14,24 @@ def test_series_one():
 def test_series_5():
     actual = fibonacci(5)
     expected = 5
+
+def test_lucas_zero():
+    actual = lucas(0)
+    expected = 2
+    assert actual == expected
+    
+def test_lucas_one():
+    actual = lucas(1)
+    expected = 1
+    assert actual == expected
+    
+def test_lucas_5():
+    actual = lucas(5)
+    expected = 11
+    assert actual == expected
+    
+def test_lucas_7():
+    actual = lucas(7)
+    expected = 29
+    assert actual == expected
+    
